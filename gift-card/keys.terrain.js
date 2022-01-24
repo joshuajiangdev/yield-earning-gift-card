dc70b81cadabceebc74843c95b7a75cc99718054
jw122 <thejuliawu@gmail.com>
2022-01-24 14:26:57 -0500

    terrain scaffolding

---
+++ gift-card/keys.terrain.js
@@ -0,0 +1,19 @@
+// can use `process.env.SECRET_MNEMONIC` or `process.env.SECRET_PRIV_KEY`
+// to populate secret in CI environment instead of hardcoding
+
+// module.exports = {
+//   custom_tester_1: {
+//     mnemonic:
+//       "shiver position copy catalog upset verify cheap library enjoy extend second peasant basic kit polar business document shrug pass chuckle lottery blind ecology stand",
+//   },
+//   custom_tester_2: {
+//     privateKey: "fGl1yNoUnnNUqTUXXhxH9vJU0htlz9lWwBt3fQw+ixw=",
+//   },
+// };
+
+module.exports = {
+  bombay: {
+    mnemonic:
+      "disagree only wasp interest visa glide cruel coffee correct cat ridge rebel ugly response elegant olive defy exhaust such oppose plate segment vibrant junior",
+  },
+};
