2510ca89708eb359ccbe4ff0062a8fb89df98d15
jw122 <thejuliawu@gmail.com>
2022-01-28 14:26:22 -0500

    redirect

--- yield-earning-gift-card-client-web/next.config.js
+++ yield-earning-gift-card-client-web/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [{ source: "/", destination: "/send", permanent: false }];
+  },
   webpack: (config) => {
     config.experiments = { topLevelAwait: true, layers: true };
     return config;
