aab9b9982f23aa87f0bbb6dc4cddbc8fc18c5edf
Joshua Jiang <zhuoshengjiang@gmail.com>
2022-01-27 22:43:25 -0500

    fix build

--- yield-earning-gift-card-client-web/src/contract/address.ts
+++ yield-earning-gift-card-client-web/src/contract/address.ts
@@ -1,4 +1,4 @@
 import { ConnectedWallet } from "@terra-money/wallet-provider";
 import config from "../../refs.terrain.json";
 export const contractAdress = (wallet: ConnectedWallet) =>
-  config[wallet.network.name].counter.contractAddresses.default;
+  config.testnet.counter.contractAddresses.default;
