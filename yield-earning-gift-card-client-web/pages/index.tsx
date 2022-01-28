1347e53db7edcef564e59dca0c382aaac1d6f25a
jw122 <thejuliawu@gmail.com>
2022-01-27 23:33:48 -0500

    basic homepage

--- yield-earning-gift-card-client-web/pages/index.tsx
+++ yield-earning-gift-card-client-web/pages/index.tsx
@@ -2,8 +2,15 @@ import Link from "next/link";
 import type { NextPage } from "next";
 import { Text } from "../src/coreui-components";
 import { useWallet, WalletStatus } from "@terra-money/wallet-provider";
-import { Button } from "../src/coreui-components/Button";
-
+import {
+  Dialog,
+  DialogTitle,
+  TextField,
+  Typography,
+  Button,
+} from "@mui/material";
+import { CardGiftcard } from "@mui/icons-material";
+import FileDownloadDoneIcon from "@mui/icons-material/FileDownloadDone";
 const Home: NextPage = () => {
   const { status, availableConnectTypes, connect, disconnect } = useWallet();
 
@@ -26,30 +33,49 @@ const Home: NextPage = () => {
   }
 
   return (
-    <ul>
-      <li>
-        <Link href="/send">
-          <a>
-            <Text>Send</Text>
-          </a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/receive">
-          <a>
-            <Text>Receive</Text>
-          </a>
-        </Link>
-      </li>
-
-      <Button
-        onClick={() => {
-          disconnect();
-        }}
+    <>
+      <Typography
+        style={{ textAlign: "center", marginTop: "10%" }}
+        variant="h2"
       >
-        Disconnect
-      </Button>
-    </ul>
+        Terragram
+      </Typography>
+
+      <Typography style={{ textAlign: "center", marginTop: "3%" }} variant="h5">
+        UST gift cards that earn yield while not spent.{" "}
+      </Typography>
+      <div style={{ textAlign: "center" }}>
+        <div style={{ padding: "5%" }}>
+          <Button variant="contained">
+            <Link href="/send">
+              <a>
+                <Text>
+                  <CardGiftcard></CardGiftcard> Send A Gift
+                </Text>
+              </a>
+            </Link>
+          </Button>
+          <Button variant="outlined">
+            <Link href="/receive">
+              <a>
+                <Text>
+                  <FileDownloadDoneIcon></FileDownloadDoneIcon> Claim
+                </Text>
+              </a>
+            </Link>
+          </Button>
+        </div>
+
+        <p>
+          {" "}
+          Built with ♥️ on{" "}
+          <img
+            src="https://www.terra.cards/wp-content/uploads/2021/07/terra-small-1.png"
+            style={{ width: "60px" }}
+          ></img>
+        </p>
+      </div>
+    </>
   );
 };
 
